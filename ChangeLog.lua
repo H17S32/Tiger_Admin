89214f398621faadcd5f9adc511f56dfd97333d3
H17S3 <105259379+H17S32@users.noreply.github.com>
2024-07-31 17:46:19 -0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,1 +1,1 @@
-Added !silentaim <ON/OFF> fixed some minor bugs with respawn, Added !gas <ME,PLAYER>, Added !bridge <ME,PLAYER> , Added !roof <ME,PLAYER>  7/28/2024
+The script Prizzlife logs your information!
