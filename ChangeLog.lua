60af4e271c50ae9853797ef5c675ac3724b6f174
H17S3 <105259379+H17S32@users.noreply.github.com>
2024-08-05 11:09:47 -0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,1 +1,1 @@
-discord.gg/tEaNKWV6SY | Added !killaura <PLAYER> with on/off feature, Added !minigun, Fixed !supergod, Added !fly, Added !unfly
+discord.gg/tEaNKWV6SY | Upgraded !spamarrest <PLAYER>, Added !carspam <ON/OFF>, !fixed crashserver (for v3.1)
