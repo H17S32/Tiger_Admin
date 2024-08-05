6459694b050fd0ffd87cfb860ca8b4bed4b2189e
H17S3 <105259379+H17S32@users.noreply.github.com>
2024-08-05 11:10:28 -0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,1 +1,1 @@
-discord.gg/tEaNKWV6SY | Upgraded !spamarrest <PLAYER>, Added !carspam <ON/OFF>, !fixed crashserver (for v3.1)
+discord.gg/tEaNKWV6SY | Upgraded !spamarrest <PLAYER>, Added !carspam <ON/OFF>, fixed !crashserver (for v3.1)
