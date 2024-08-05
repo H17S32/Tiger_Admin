56a328dd283cbf6e200100b1753611b961d39bd2
H17S3 <105259379+H17S32@users.noreply.github.com>
2024-08-04 22:33:48 -0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,1 +1,1 @@
-(V3.1 IS NEWEST IF YOU GOT OLDER ITS VERY OUTDATED!) Added !killaura <PLAYER> with on/off feature, Added !minigun, Fixed !supergod, Added !fly, Added !unfly
+discord.gg/tEaNKWV6SY | Added !killaura <PLAYER> with on/off feature, Added !minigun, Fixed !supergod, Added !fly, Added !unfly
