de7f6d16f061aa06331d3e02f763a05705a7e470
H17S3 <105259379+H17S32@users.noreply.github.com>
2024-08-05 17:16:27 -0700

    Update ChangeLog.lua

--- ChangeLog.lua
+++ ChangeLog.lua
@@ -1,1 +1,1 @@
-discord.gg/tEaNKWV6SY | Upgraded !spamarrest <PLAYER>, Added !carspam <ON/OFF>, fixed !crashserver (for v3.1), Fixed !fly for appleware
+YOUR TIGER ADMIN IS VERY OUTDATED! | discord.gg/tEaNKWV6SY
